b9d047b37143ef18cb1c6ea6ab8073e4a18709f4
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 10:16:07 -0300

    Fix formatting in simple_operations_calculator.c
    Google format convention

--- c-exercicios/simple_operations_calculator.c
+++ c-exercicios/simple_operations_calculator.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int Main()
 {
-  printf("Add %d and %d result is: %d\n", 2, 1, 2+1 );
-  printf("Subtract %d and %d result is: %d\n", 2, 1, 2-1 );
-  printf("Multiply %d and %d result is: %d\n", 2, 1, 2*1 );
-  printf("Divide %d and %d result is: %d\n", 2, 1, 2/1 );
-  printf("Remainder %d and %d result is: %d\n", 2, 1, 2%1 );
+  printf("Add %d and %d result is: %d\n", 2, 1, 2 + 1);
+  printf("Subtract %d and %d result is: %d\n", 2, 1, 2 - 1);
+  printf("Multiply %d and %d result is: %d\n", 2, 1, 2 * 1);
+  printf("Divide %d and %d result is: %d\n", 2, 1, 2 / 1);
+  printf("Remainder %d and %d result is: %d\n", 2, 1, 2 % 1);
   return 0;
 }
