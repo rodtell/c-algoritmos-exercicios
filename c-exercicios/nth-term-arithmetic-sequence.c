7a1247cfb5f84084a9dfe76480b148add9e4532e
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 10:00:03 -0300

    Refactor variable names for consistency
    Google format convention

--- c-exercicios/nth-term-arithmetic-sequence.c
+++ c-exercicios/nth-term-arithmetic-sequence.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int Main()
 {
-  int firstTerm;
-  int nTerm;
-  int differenceBetweenTerms;
-  int lengthSequence;
+  int first_term;
+  int n_term;
+  int difference_between_terms;
+  int length_sequence;
 
   printf("Type the first term of the sequence: \n");
-  scanf("%d", &firstTerm);
+  scanf("%d", &first_term);
   printf("Type the difference between the terms of the sequence: \n");
-  scanf("%d", &differenceBetweenTerms);
+  scanf("%d", &difference_between_terms);
   printf("Type the length of the sequence: \n");
-  scanf("%d", &lengthSequence);
+  scanf("%d", &length_sequence);
 
-  nTerm = firstTerm + (lengthSequence - 1) * differenceBetweenTerms;
-  printf("The n-th term of the sequence is: %d\n",nTerm );
+  n_term = first_term + (length_sequence - 1) * difference_between_terms;
+  printf("The n-th term of the sequence is: %d\n", n_term);
     
   return 0;
 }
