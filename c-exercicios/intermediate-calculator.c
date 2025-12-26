5f1e968fa7107b617c5b4dae34faffa35f2783c5
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 09:39:21 -0300

    Refactor variable names for consistency
    Google format convention

--- c-exercicios/intermediate-calculator.c
+++ c-exercicios/intermediate-calculator.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int Main()
 {
-  int firstNumber;
-  int secondNumber;
+  int first_number;
+  int second_number;
   char operation;
 
-  printf("Input first number: \n" );
-  scanf("%d", &firstNumber);
-  printf("Input second number: \n" );
-  scanf("%d", &secondNumber);
+  printf("Input first number: \n");
+  scanf("%d", &first_number);
+  printf("Input second number: \n");
+  scanf("%d", &second_number);
   printf("Select operation '+' '-' '*' '/' '%%' \n");
   getchar();
   scanf("%c", &operation);
 
   switch (operation) {
   case '+': {
-    printf("The sum of %d and %d is: %d \n",firstNumber, secondNumber, firstNumber+secondNumber );
+    printf("The sum of %d and %d is: %d \n", first_number, second_number, first_number + second_number);
     break;
   }
   case '-': {
-    printf("The subtraction of %d and %d is: %d \n",firstNumber, secondNumber, firstNumber-secondNumber );
+    printf("The subtraction of %d and %d is: %d \n", first_number, second_number, first_number - second_number);
     break;
   }
   case '*': {
-    printf("The multiplication of %d and %d is: %d \n",firstNumber, secondNumber, firstNumber*secondNumber );
+    printf("The multiplication of %d and %d is: %d \n", first_number, second_number, first_number * second_number);
     break;
   }
 
   case '/': {
-    printf("The division of %d and %d is: %d \n",firstNumber, secondNumber, firstNumber/secondNumber );
+    printf("The division of %d and %d is: %d \n", first_number, second_number, first_number / second_number);
     break;
   }
   case '%': {
-    printf("The remainer of %d and %d is: %d \n",firstNumber, secondNumber, firstNumber%secondNumber );
+    printf("The remainer of %d and %d is: %d \n", first_number, second_number, first_number % second_number);
     break;
   }
   default:
