c21b5cf38a24bbae9fe30835729c245b021065f5
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 08:58:11 -0300

    Format correction
    Spacing on line 16

--- c-exercicios/date-in-hex.c
+++ c-exercicios/date-in-hex.c
@@ -13,7 +13,7 @@ int Main()
   printf("Input day:\n");
   scanf("%d", &day);
 
-  printf("%d year, %d month, and %d day in hex is: 0x%x year, 0x%x month, 0x%x day\n",year, month, day, year, month, day );
+  printf("%d year, %d month, and %d day in hex is: 0x%x year, 0x%x month, 0x%x day\n",year, month, day, year, month, day);
 
   return 0;
 }
