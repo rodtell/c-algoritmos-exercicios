8de0817e236a57bacb5dd60d22db23ae13029724
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 10:18:00 -0300

    Refactor variable names for consistency
    Google format convention

--- c-exercicios/sum-of-sequence.c
+++ c-exercicios/sum-of-sequence.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int Main()
 {
-  int firstValue;
-  int lastValue;
-  int sequenceLength;
-  int sumOfSequence;
+  int first_value;
+  int last_value;
+  int sequence_length;
+  int sum_of_sequence;
 
   printf("Type first value: \n");
-  scanf("%d", &firstValue);
+  scanf("%d", &first_value);
   printf("Type last value: \n");
-  scanf("%d", &lastValue);
+  scanf("%d", &last_value);
   printf("Type sequence length: \n");
-  scanf("%d", &sequenceLength);
+  scanf("%d", &sequence_length);
 
-  sumOfSequence = (firstValue + lastValue) * sequenceLength / 2;
-  printf("The sum of the arithmetic sequence is: %d\n",sumOfSequence );
+  sum_of_sequence = (first_value + last_value) * sequence_length / 2;
+  printf("The sum of the arithmetic sequence is: %d\n",sum_of_sequence);
 
   return 0;
 }
