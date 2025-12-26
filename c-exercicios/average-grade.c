ff6dc539beeed6cc4e83209ebf6f7854e13dc188
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 08:55:55 -0300

    Refactor variable names for grade inputs
    Google format convention.

--- c-exercicios/average-grade.c
+++ c-exercicios/average-grade.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int Main()
 {
-    int gradeOne;
-    int gradeTwo;
-    int gradeThree;
+    int grade_one;
+    int grade_two;
+    int grade_three;
     double average;
 
     printf("Type in grade one: \n");
-    scanf("%d", &gradeOne);
+    scanf("%d", &grade_one);
     printf("Type in grade two: \n");
-    scanf("%d", &gradeTwo);
+    scanf("%d", &grade_two);
     printf("Type in grade three: \n");
-    scanf("%d", &gradeThree);
-    average = ((double)gradeOne+gradeTwo+gradeThree)/3;
-    printf("Average of the 3 grades: %.1f\n",average );
+    scanf("%d", &grade_three);
+    average = ((double)grade_one + grade_two + grade_three) / 3;
+    printf("Average of the 3 grades: %.1f\n", average);
 
     return 0;
 }
