d319ac3f5ccfa24c77a9903bc799cc28a24384f5
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 10:40:04 -0300

    Update unique-clock-representation.c
    Google format convention

--- c-exercicios/unique-clock-representation.c
+++ c-exercicios/unique-clock-representation.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-  int secondsTotal;
+int Main() {
+  int seconds_total;
   int hours;
   int minutes;
   int seconds;
 
   printf("Input total seconds\n");
-  scanf("%d", &secondsTotal);
+  scanf("%d", &seconds_total);
 
-  hours = secondsTotal / 3600;
-  minutes = (secondsTotal / 60) % 60;
-  seconds = secondsTotal % 60;
-  printf("Total seconds converted in to clock: %dh%dm%ds \n",hours, minutes, seconds );
+  hours = seconds_total / 3600;
+  minutes = (seconds_total / 60) % 60;
+  seconds = seconds_total % 60;
+  printf("Total seconds converted in to clock: %dh%dm%ds \n", hours, minutes, seconds);
   
   return 0;
 }
