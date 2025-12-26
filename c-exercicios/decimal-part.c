eeae67d259b8d3082bbb84aa1678970fcb639bb3
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 08:57:32 -0300

    Fix function name from main to Main
    Google format convention.

--- c-exercicios/decimal-part.c
+++ c-exercicios/decimal-part.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int Main()
 {
   float data;
   
   printf("Input data: \n");
   scanf("%f", &data);
-  printf("Only decimal part of the number is: %.2f\n",data - (int)data );
+  printf("Only decimal part of the number is: %.2f\n",data - (int)data);
   
   return 0;
 }
