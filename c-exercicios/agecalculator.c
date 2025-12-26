06a9ff179067ca225e624a46e3d5256a18034569
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 08:59:15 -0300

    Fix formatting in agecalculator.c
    Line 6 spacing.

--- c-exercicios/agecalculator.c
+++ c-exercicios/agecalculator.c
@@ -3,7 +3,7 @@
 int Main()
 {
   printf("Current year: %d\n", 2024);
-  printf("Year you were born: %d\n", 1979 );
+  printf("Year you were born: %d\n", 1979);
   printf("Your age: %d\n", 2024-1979);
   return 0;
 }
