7d4fbde424a0624ff6eec01c232b38da3ae208fa
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 09:01:52 -0300

    Refactor variable names for distance calculation
    Google format convetion.

--- c-exercicios/distance-between-two-points.c
+++ c-exercicios/distance-between-two-points.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(int argc, char *argv[])
+int Main()
 {
-  int xOne;
-  int yOne;
-  int xTwo;
-  int yTwo;
+  int x_one;
+  int y_one;
+  int x_two;
+  int y_two;
   int distance;
 
   printf("Input x1: \n");
-  scanf("%d", &xOne);
+  scanf("%d", &x_one);
   printf("Input y1: \n");
-  scanf("%d", &yOne);
+  scanf("%d", &y_one);
   printf("Input x2: \n");
-  scanf("%d", &xTwo);
+  scanf("%d", &x_two);
   printf("Input y2: \n");
-  scanf("%d", &yTwo);
+  scanf("%d", &y_two);
 
-  distance = sqrt(pow((xOne - xTwo), 2) + pow((yOne - yTwo), 2));
-  printf("The distance between two points is: %d\n",distance );
+  distance = sqrt(pow((x_one - x_two), 2) + pow((y_one - y_two), 2));
+  printf("The distance between two points is: %d\n", distance);
   
   return 0;
 }
