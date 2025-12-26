3ffbe088e008f54d29999d81e3d8cfd9a3df73a4
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 09:51:28 -0300

    Refactor variable names and improve formatting
    Google format convention

--- c-exercicios/minimum-maximum-three-numbers.c
+++ c-exercicios/minimum-maximum-three-numbers.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int Main()
 {
-  int numberOne;
-  int numberTwo;
-  int numberThree;
+  int number_one;
+  int number_two;
+  int number_three;
   int maximum;
   int minimum;
 
   printf("Input first number: \n");
-  scanf("%d", &numberOne);
+  scanf("%d", &number_one);
   printf("Input second number: \n");
-  scanf("%d", &numberTwo);
+  scanf("%d", &number_two);
   printf("Input third number: \n");
-  scanf("%d", &numberThree);
+  scanf("%d", &number_three);
 
-  maximum = numberOne;
-  minimum = numberTwo;
+  maximum = number_one;
+  minimum = number_two;
 
-  if (numberTwo > numberOne) {
-    maximum = numberTwo;
-    minimum = numberOne;
+  if (number_two > number_one) {
+    maximum = number_two;
+    minimum = number_one;
   }
-  if (numberThree > maximum) {
-    maximum = numberThree;
+  if (number_three > maximum) {
+    maximum = number_three;
   }
-  if (minimum > numberThree) {
-    minimum = numberThree;
+  if (minimum > number_three) {
+    minimum = number_three;
   }
 
-  printf("The maximum number is %d and the minimum number is %d\n",maximum, minimum );
+  printf("The maximum number is %d and the minimum number is %d\n", maximum, minimum);
     
   return 0;
 }
