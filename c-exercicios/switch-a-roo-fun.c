d8e0465ea29868a4c96eb190be76e88a8efcea61
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 10:23:07 -0300

    Refactor variable names for consistency
    Google format convention

--- c-exercicios/switch-a-roo-fun.c
+++ c-exercicios/switch-a-roo-fun.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int Main()
 {
-  float numberA = 3.5;
-  float numberB = 5.6;
-  float numberHold;
+  float number_a = 3.5;
+  float number_b = 5.6;
+  float number_hold;
 
-  printf("Number A is %.2f\n",numberA );
-  printf("Number B is %.2f\n",numberB );
-  numberHold = numberA;
-  numberA = numberB;
-  numberB = numberHold;
+  printf("Number A is %.2f\n", number_a);
+  printf("Number B is %.2f\n", number_b);
+  number_hold = number_a;
+  number_a = number_b;
+  number_b = number_hold;
   printf("After swap is: \n");
-  printf("Number A is %.2f\n",numberA );
-  printf("Number B is %.2f\n",numberB );
+  printf("Number A is %.2f\n", number_a);
+  printf("Number B is %.2f\n", number_b);
   
   return 0;
 }
