e5fb2f743a6fae0d7811be3aa27f407f35988b3e
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 09:40:22 -0300

    Fix formatting in leap year output messages
    Google format convention

--- c-exercicios/leap-year.c
+++ c-exercicios/leap-year.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int Main()
 {
   int year;
 
@@ -9,9 +9,9 @@ int main(int argc, char *argv[])
   scanf("%d", &year);
 
   if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
-    printf("%d is a leap year\n",year );
+    printf("%d is a leap year\n", year);
   } else {
-    printf("%d is not a leap year\n",year );
+    printf("%d is not a leap year\n", year);
   }
 
   return 0;
