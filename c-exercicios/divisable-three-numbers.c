a1a3384d4173c22f2262caee6c6568ab0e3683d1
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 09:05:16 -0300

    Refactor variable names for consistency
    Google format convention.

--- c-exercicios/divisable-three-numbers.c
+++ c-exercicios/divisable-three-numbers.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int Main()
 {
-  int numberOne;
-  int numberTwo;
-  int numberThree;
+  int number_tne;
+  int number_two;
+  int number_three;
 
   printf("Input first number\n");
-  scanf("%d", &numberOne);
+  scanf("%d", &number_one);
   printf("Input second number\n");
-  scanf("%d", &numberTwo);
+  scanf("%d", &number_two);
   printf("Input third number\n");
-  scanf("%d", &numberThree);
+  scanf("%d", &number_three);
   
-  if (((numberOne % numberTwo == 0) || (numberTwo % numberOne == 0)) && ((numberTwo % numberThree == 0) || (numberThree % numberTwo ==0)) && ((numberOne % numberThree == 0) || (numberThree % numberOne == 0))) {
+  if (((number_one % number_two == 0) || (number_two % number_one == 0)) && ((number_two % number_three == 0) || (number_three % number_two ==0)) && ((number_one % number_three == 0) || (number_three % number_one == 0))) {
     printf("Divisible\n");
   } else {
     printf("Not divisible\n");
   }
 
-  if (numberOne == 0 || numberTwo == 0 || numberThree == 0) {
+  if (number_one == 0 || number_two == 0 || number_three == 0) {
     printf("Not possible to divide\n");
   }
   
