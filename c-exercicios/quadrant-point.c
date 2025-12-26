15f63e850ef7ccf8c05605c93aa30d4117acf486
Rodrigo Telles <77083219+rodtell@users.noreply.github.com>
2025-12-26 10:12:13 -0300

    Refactor variable names for clarity
    Google format convention

--- c-exercicios/quadrant-point.c
+++ c-exercicios/quadrant-point.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int Main()
 {
-  int xValue;
-  int yValue;
+  int x_value;
+  int y_value;
 
   printf("Input value of x\n");
-  scanf("%d", &xValue);
+  scanf("%d", &x_value);
   printf("Input value of y\n");
-  scanf("%d", &yValue);
+  scanf("%d", &y_value);
 
-  if (xValue >= 0 && yValue >=0) {
+  if (x_value >= 0 && y_value >=0) {
     printf("1st quadrant\n");
-  } else if (xValue < 0 && yValue >=0) {
+  } else if (x_value < 0 && y_value >=0) {
     printf("2nd quadrant\n");
-  } else if (xValue < 0 && yValue < 0) {
+  } else if (x_value < 0 && y_value < 0) {
     printf("3rd quadrant\n");
   } else {
     printf("4th quadrant\n");
